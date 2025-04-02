0de2fb48982494dc3967cfe818c97b33f3e0bbee
vanindm <dmvanin@edu.hse.ru>
2025-04-02 18:35:15 +0300

    Добавлена сортировка полигонов

--- torrus.py
+++ torrus.py
@@ -107,9 +107,11 @@ def drawGrid():
 def draw():
     canvas.delete("all")
     drawGrid()
+    global polys
+    polys = sorted(polys, key = lambda x : tMat.dot(np.append(x[0], 1))[2])
     for poly in polys:
         polyProj = list(map(lambda x : tMat.dot(np.append(x, 1)[:, np.newaxis]), poly))
-        canvas.create_polygon(int(polyProj[0][0] + WIDTH / 2), int(polyProj[0][1] + HEIGHT / 2), int(polyProj[1][0] + WIDTH / 2), int(polyProj[1][1] + HEIGHT / 2), int(polyProj[2][0] + WIDTH / 2), int(polyProj[2][1] + HEIGHT / 2), fill='#0000FF')
+        canvas.create_polygon(int(polyProj[0][0] + WIDTH / 2), int(polyProj[0][1] + HEIGHT / 2), int(polyProj[1][0] + WIDTH / 2), int(polyProj[1][1] + HEIGHT / 2), int(polyProj[2][0] + WIDTH / 2), int(polyProj[2][1] + HEIGHT / 2), fill='#0000FF', outline='#000000')
 
 draw()
 
